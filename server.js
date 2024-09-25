6b639f4c092e06b342f5019e8911ec7c8cbe6b58
Roni-Boiz <2019cs155@stu.ucsc.lk>
2024-09-25 17:36:07 +0530

    update backend code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
